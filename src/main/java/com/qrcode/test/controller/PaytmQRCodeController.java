d15e7787e72dc013bf5d5ce9b3068faf7d927b0c
mohamed <marafath07@gmail.com>
2022-10-26 19:52:03 +0530

    removed unwanted code

--- src/main/java/com/qrcode/test/controller/PaytmQRCodeController.java
+++ src/main/java/com/qrcode/test/controller/PaytmQRCodeController.java
@@ -27,24 +27,4 @@ public class PaytmQRCodeController {
 		model.addAttribute("code", qrCodePath);
 		return "QRcode";
 	}
-
-	/**@GetMapping("/readQR")
-	public String verifyQR(@RequestParam("qrImage") String qrImage, Model model) throws Exception {
-	model.addAttribute("content", readQR(qrImage));
-	model.addAttribute("code", qrImage);
-	return "QRcode";
-	}*/
-
-	/**private String readQR(String qrImage) throws Exception {
-		final Resource fileResource = resourceLoader.getResource("classpath:static/" + qrImage);
-		File QRfile = fileResource.getFile();
-		BufferedImage bufferedImg = ImageIO.read(QRfile);
-		LuminanceSource source = new BufferedImageLuminanceSource(bufferedImg);
-		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
-		Result result = new MultiFormatReader().decode(bitmap);
-		System.out.println("Barcode Format: " + result.getBarcodeFormat());
-		System.out.println("Content: " + result.getText());
-		return result.getText();
-
-	}*/
 }
